c9299b54cbb7c3c33fe2c3c18b1a9c10868534ce
Utkarsh <utkarsh.v1901@gmail.com>
2022-08-03 01:19:54 +0530

    backend routes for login signup and middlewares

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+connectDB();
+const port = process.env.PORT ||  5000;
+
+const app = express()
+
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
+
+app.use(errorHandler)
+
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
