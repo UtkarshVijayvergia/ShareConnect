5644691f7dda97501ddf5b9e5566e56c0ad5f480
Utkarsh <utkarsh.v1901@gmail.com>
2022-12-29 01:57:44 +0530

    added navigation to profile details

--- frontend/src/pages/profile/profileComponents/User.jsx
+++ frontend/src/pages/profile/profileComponents/User.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import './user.css'
+import { Link } from 'react-router-dom'
 
 const User = (props) => {
-  return (
-    <div>
-        <h4 className='user'>{props.name}</h4>
-        <h4>{props.email}</h4>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+                <p className='name text-decor'>{props.name}</p>
+                <p className='email text-decor'>{props.email}</p>
+            </div>
+            <div className='btn btn-sm btn-success'>
+                <Link className='navigate' to="">Edit Profile Details</Link>
+            </div>
+        </div>
+    )
 }
 
 export default User
