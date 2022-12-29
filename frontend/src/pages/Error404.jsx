52edbadfa0a776e1ac43369604f9fb91ebe69b55
Utkarsh <utkarsh.v1901@gmail.com>
2022-12-29 20:42:00 +0530

    Error component for link not found

--- frontend/src/pages/Error404.jsx
+++ frontend/src/pages/Error404.jsx
@@ -3,9 +3,9 @@ import './error404.css';
 
 const Error404 = () => {
     return (
-        <div>
-            <h1>Error 404</h1>
-            <p>Sorry, the page you were looking for could not be found.</p>
+        <div className='body'>
+            <h1 className='header'>Error 404</h1>
+            <p className='subHeader'>Sorry, the page you were looking for could not be found.</p>
         </div>
     );
 };
