b88b472e8ac6742e0eefe6801325a1dcb4b51ca2
Utkarsh <utkarsh.v1901@gmail.com>
2022-12-29 20:37:41 +0530

    user component UI bug fix

--- frontend/src/pages/profile/profileComponents/User.jsx
+++ frontend/src/pages/profile/profileComponents/User.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom'
 const User = (props) => {
     return (
         <div>
-            <div>
-                <p className='name text-decor'>{props.name}</p>
-                <p className='email text-decor'>{props.email}</p>
+            <div className='text-decor'>
+                <p className='name'>{props.name}</p>
+                <p className='email'>{props.email}</p>
             </div>
             <div className='btn btn-sm btn-success'>
-                <Link className='navigate' to="">Edit Profile Details</Link>
+                <Link className='navigate' to={`/user/${props.name}/${props._id}`}>Edit Profile Details</Link>
             </div>
         </div>
     )
