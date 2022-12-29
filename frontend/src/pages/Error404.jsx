ccbba0411307765e54c3a49452ef17c6a869d6d3
Utkarsh <utkarsh.v1901@gmail.com>
2022-12-29 12:46:20 +0530

    Error404 component

---
+++ frontend/src/pages/Error404.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './error404.css';
+
+const Error404 = () => {
+    return (
+        <div>
+            <h1>Error 404</h1>
+            <p>Sorry, the page you were looking for could not be found.</p>
+        </div>
+    );
+};
+export default Error404
