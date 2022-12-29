c2845d2b20fb3231a42e0f6018144caec9b717fe
Utkarsh <utkarsh.v1901@gmail.com>
2022-12-29 20:36:03 +0530

    accountDetails component

---
+++ frontend/src/pages/profileDetails/AccountDetails.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import './accountDetails.css'
+import AccountIntroDetails from './accountDetailsComponents/AccountIntroDetails'
+
+const AccountDetails = () => {
+    const { user } = useSelector((state) => state.auth)
+    return (
+
+        <div className='univ'>
+            <div className="cardcontainer card">
+                <div className='accountDetails-header'>
+                    <p className='mainHeading'>Edit My Profile</p>
+                    <div className='btn-placement'>
+                        <Link className='btn btn-lg btn-primary btn-edit' to={`/user/${user.name}`}>View Profile</Link>
+                    </div>
+                </div>
+                <hr />
+                <AccountIntroDetails 
+                    name = {user.name}
+                />
+            </div>
+        </div>
+    )   
+}
+
+export default AccountDetails
