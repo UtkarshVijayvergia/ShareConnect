0b11c818a6d9d2b2ae6c1be51374263b86059f0a
Utkarsh <utkarsh.v1901@gmail.com>
2023-05-22 01:27:32 +0530

    profilePicMiddleware module renamed

--- backend/routes/userProfilePicRoutes.js
+++ backend/routes/userProfilePicRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {  getProfilePic, setProfilePic } = require('../controller/userProfilePicController')
 const { protect } = require('../middleware/authMiddleware')
-const { upload } = require('../middleware/imageMiddleware')
+const { upload } = require('../middleware/profilePicMiddleware')
 
 // Configure cors
 const cors = require('cors');
