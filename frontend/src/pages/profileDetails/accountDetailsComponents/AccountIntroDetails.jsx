f5b0153671788bca3d14bbf7c5b85943aca12140
Utkarsh <utkarsh.v1901@gmail.com>
2023-05-22 01:21:00 +0530

    image upload API bug fix

--- frontend/src/pages/profileDetails/accountDetailsComponents/AccountIntroDetails.jsx
+++ frontend/src/pages/profileDetails/accountDetailsComponents/AccountIntroDetails.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { useRef } from 'react';
 import { useSelector } from 'react-redux';
 import './accountIntroDetails.css'
 
 const AccountIntroDetails = (props) => {
     const { user } = useSelector((state) => state.auth)
-    // const fileInput = useRef(null);
 
-
-    const [imageUrl, setImageUrl] = useState(null);
-    const [imageFile, setImageFile] = useState({});
+    const [profilePicUrl, setProfilePicUrl] = useState();
+    const [profilePic, setProfilePic] = useState();
 
 
     // Get user profile pic
@@ -31,60 +28,60 @@ const AccountIntroDetails = (props) => {
         }
     }
 
-
+    // Get user profile pic - UseEffect
     useEffect(() => {
         (async () => {
             const url = await getImage();
-            setImageUrl(url);
+            setProfilePicUrl(url);
         })();
     }, []);
 
 
-    // const handleClick = () => {
-    //     fileInput.current.click();
-    // };
+    // Read contents of Profile pic
+    const handleChange = (e) => {
+        const newProfilePic = e.target.files[0];
+        const reader = new FileReader();
+      
+        reader.onloadend = () => {
+            setProfilePic(newProfilePic);
+        };
+      
+        if(newProfilePic){
+            reader.readAsDataURL(newProfilePic);
+        }
+      };
 
-    const handleChange = e => {
-        const file = e.target.files[0];
-        setImageFile(file);
-        // reader.readAsDataURL(file);
-        // console.log(file.type);
-    };
 
+    // Change profile pic
     const submit = async (e) => {
         e.preventDefault();
-        // const file = e.target.files[0];
         const formData = new FormData();
-        formData.append('file', imageFile);
-        // formData.append('otherData', 'some value');
-        console.log(formData.get('file'));
-        console.log("try");
+        formData.append('profilePic', profilePic);
+
         const response = await fetch(`http://localhost:5000/api/user/profilepic/${user._id}`, {
             method: "POST",
             body: formData,
             headers:{
                 Authorization: `Bearer ${user.token}`,
-                'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>',
+                // 'Content-Type': 'multipart/form-data; boundary=<calculated when request is sent>',
             },
-            // data: formData,
         })
+        
         if (!response.ok) {
             console.error(response.statusText);
         } 
         else {
-            const data = await response.json();
-            console.log(data);
+            await response.json();
         }
     };
 
-        // console.log("qwertyujhbv");
-    // }
 
     return (
         <div>
             <div className='head1'>
                 <p className='head-intro'>Introduction</p>
                 <p className='head-p'>Let the ShareConnect community recognize you.</p>
+                
                 <div className='info'>
                     <div className='namer'>
                         <p className='namer-name'>Name </p>
@@ -93,25 +90,27 @@ const AccountIntroDetails = (props) => {
                     <br />
                     <div className='profile-picture'>
                         <p className='namer-profile'>Profile Picture </p>
+
                         <div className='card accountPicContainer'>
-                        {/* src={require('../../../images/profileImages/bakugou.jpg')} */}
-                            <img className='accountPic' src={imageUrl} alt="Profile Picture" />
+                            <img className='accountPic' src={profilePicUrl} alt="Profile Picture" />
                         </div>
+
                         <div>
-                            <form onSubmit={submit}>
+                            <form encType="multipart/form-data">
                                 <div className='profilePic-upload'>
-                                    {/* <button className='btn btn-primary btn-edit-sm' onClick={handleClick}>Choose a file</button> */}
                                     <div className='profilePic-size'>
-                                        <p>Maximum size of 1MB. JPG, JPEG, or PNG.</p>
+                                        <p>
+                                            Maximum size of 1MB. JPG, JPEG, or PNG.
+                                        </p>
                                     </div>
-                                    <input type="file" onChange={handleChange}  />
+                                    <input type="file" name="profilePic" onChange={handleChange}  />
                                     <br />
-                                    <button className='btn btn-sm btn-primary'>change</button>
+                                    <button onClick={submit} className='btn btn-sm btn-primary'>change</button>
                                 </div>
                             </form>
                         </div>
-                    </div>
-                    <br />
+
+                    </div><br />
                 </div>
             </div>
         </div>
