fdb454b1e838493a27c37c3a15a3458bf3902835
Utkarsh <utkarsh.v1901@gmail.com>
2023-05-22 18:58:41 +0530

    login register imports fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
 import Dashboard from './pages/Dashboard';
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
 import Navbar from './components/Navbar';
 import About from './pages/About';
 import Profile from './pages/profile/Profile';
