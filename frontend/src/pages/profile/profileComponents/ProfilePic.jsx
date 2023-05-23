356d540c5efc6a463db4052f0ec66aefd443b218
Utkarsh <utkarsh.v1901@gmail.com>
2023-05-23 17:50:05 +0530

    profile pic bug fix

--- frontend/src/pages/profile/profileComponents/ProfilePic.jsx
+++ frontend/src/pages/profile/profileComponents/ProfilePic.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import './profilePic.css'
 
 const ProfilePic = () => {
+    const { user } = useSelector((state) => state.auth)
+    const [profilePicUrl, setProfilePicUrl] = useState();
+
+    // Get user profile pic
+    const getImage = async () => {
+        try{
+            const response = await fetch(`http://localhost:5000/api/user/profilepic/${user._id}`, {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                    'Content-Type': 'application/json',
+                },
+            });
+            const blob = await response.blob();
+            const url = URL.createObjectURL(blob);
+            return url;
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+
+    // Get user profile pic - UseEffect
+    useEffect(() => {
+        const fetchImageUrl = async () => {
+          const url = await getImage();
+          setProfilePicUrl(url);
+        };
+      
+        fetchImageUrl();
+    }, []);
+
     return (
         <div>
             <div className='card profilePicContainer'>
-                <img className='profilePic' src={require('../../../images/profileImages/bakugou.jpg')} alt="" />
+                <img className='profilePic' src={profilePicUrl} alt="Profile Picture" />
             </div>
         </div>
     )
