f4b78cc55e213d6f607d3fa51f201534d136c6c8
Utkarsh <utkarsh.v1901@gmail.com>
2024-01-02 20:48:52 +0530

    userPost Schema updated

---
+++ backend/models/userPostModel.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose')
+
+const userPostSchema = mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        default: "",
+    },
+    body: {
+        type: String,
+        default: "",
+    },
+    media: [{
+        image: {
+            data: Buffer,
+            contentType: String,
+        },
+        video: {
+            data: Buffer,
+            contentType: String,
+        },
+    }],
+    // tags: {
+    // },
+    likes: [{
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        created_at: {
+            type: Date,
+            default: Date.now,
+            required: true,
+        },
+    }],
+    comments: [{
+        _id: mongoose.Schema.Types.ObjectId,
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        text: {
+            type: String,
+            required: true 
+        },
+        created_at: {
+            type: Date,
+            default: Date.now,
+            required: true,
+        },
+    }],
+},
+{
+    timestamps: true
+});
+
+
+module.exports = mongoose.model('user_post', userPostSchema)
